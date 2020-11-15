6e07d09b2c6d31225bc4f3e9eef2b2389265d268
HeizRaum <lualquigo@gmail.com>
2020-11-15 16:15:00 -0500

    Add: The Sqlite3 package.

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ application.get('/countries', (request, response) => {
   response.sendFile(__dirname + '/public/countries.html');
 });
 
+application.get('/names', (request, response) => {
+  response.sendFile(__dirname + '/public/names.html');
+  console.log(request.route);
+});
+
+application.get('/cities', (request, response) => {
+  response.sendFile(__dirname + '/public/cities.html');
+});
+
 application.listen(port, () => {
   console.log(`Application started at port: ${port}`);
 });
